f75d201c43573577d61b8285b4f9f7eb31f08193
jin237 <fgdaojebvoa237@gmail.com>
2024-01-15 01:44:57 +0900

    solanaのステーキング数量の取得

--- ethereum/solidity/hello.sol
+++ ethereum/solidity/hello.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.8.23;
+
 contract HelloWorld {
     string public greet = "Hello World!";
 }
---
+++ solana/getBalance.ts
@@ -0,0 +1,24 @@
+import * as web3 from '@solana/web3.js';
+
+async function getBalance() {
+    // Solanaクラスタ（ネットワーク）を設定
+    const connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
+
+    // 残高を確認したいアドレスを設定
+    const address = new web3.PublicKey('EDuVpfE29Rb7S9q1bM5Db8WwtqMAPbZb8bqrfNcNt24c');
+
+    try {
+        // 残高を取得
+        const balance = await connection.getBalance(address);
+        
+        console.log(web3.LAMPORTS_PER_SOL);
+
+        // 単位をSOLに変換して表示
+        console.log(`アドレス: ${address}`)
+        console.log(`残　　高: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+    } catch (error) {
+        console.error('残高取得ができませんでした。エラー詳細:', error);
+    }
+}
+
+getBalance();
